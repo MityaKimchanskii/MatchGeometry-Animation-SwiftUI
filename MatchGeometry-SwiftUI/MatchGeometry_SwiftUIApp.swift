b3f260992fcc361bab96801972a5280723423417
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-26 23:30:52 -0700

    Initial Commit

---
+++ MatchGeometry-SwiftUI/MatchGeometry_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGeometry_SwiftUIApp.swift
+//  MatchGeometry-SwiftUI
+//
+//  Created by Mitya Kim on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MatchGeometry_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
